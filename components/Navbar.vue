02bd522cd02f107f3e50eacd4bd8728b20f9df33
ezhumalaip <ezhumalaip107@gmail.com>
2022-06-11 18:42:53 +0530

    add folders

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,22 +1,27 @@
 <template>
   <section>
-    <div>
-      <h1 class="navbar-brand" @click="routeToHomepage">
+    <div class="logo-titel">
+      <h1 class="navbar-brand">
         <span class="logo-newlogo">STU Innovations</span>
       </h1>
     </div>
-    <nav id="menu" style="float: right">
-      <input
-        type="checkbox"
-        id="responsive-menu"
-        onclick="updatemenu()"
-      /><label></label>
+    <nav id="menu" class="nav-menu-items" style="float:left">
+      <input type="checkbox" id="responsive-menu" onclick="updatemenu()" />
+
+      <label></label>
 
       <ul>
-       <li><NuxtLink class="dropdown-arrow" to="/Platform">Platform</NuxtLink></li>
-        <li><NuxtLink class="dropdown-arrow" to="/services">Services</NuxtLink></li>
-        <li><NuxtLink class="dropdown-arrow" to="/testimonials">Testimonials</NuxtLink></li>
-        
+        <li>
+          <NuxtLink class="dropdown-arrow" to="/Platform">Platform</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink class="dropdown-arrow" to="/services">Services</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink class="dropdown-arrow" to="/testimonials"
+            >Testimonials</NuxtLink
+          >
+        </li>
 
         <li>
           <a class="dropdown-arrow" href="#">Company</a>
@@ -24,24 +29,35 @@
             <li><a href="#">About</a></li>
             <li><a href="#">Careers</a></li>
             <li><NuxtLink to="/contact">Contact Us</NuxtLink></li>
-			<li><a href="#"></a></li>
+            <li><a href="#"></a></li>
           </ul>
         </li>
       </ul>
     </nav>
+    <div class="mobile-titel">
+      <h1 class="navbar-brand">
+        <span class="logo-newlogo">STU Innovations</span>
+      </h1>
+    </div>
   </section>
 </template>
-<script>
-
-
-  
-</script>
+<script></script>
 <style>
 #menu {
   background: #ffffff;
   color: rgb(0, 0, 0);
   height: 75px;
 }
+.nav-menu-items {
+  margin-left: 400px;
+}
+.logo-titel {
+  display: block;
+}
+.mobile-titel {
+  display: none;
+}
+
 #menu ul,
 #menu li {
   margin: 9px auto;
@@ -55,6 +71,7 @@
 .logo-newlogo {
   color: #ff50ac;
   margin-top: 5px;
+  padding-left: 17px;
 }
 #menu li {
   float: left;
@@ -93,9 +110,9 @@
   left: 35px;
 }
 #menu label:before {
-  font-size: 1.6em;
+  font-size: 2.5em;
   content: "\2261";
-  margin-left: 20px;
+  margin-left: 2px;
 }
 .navbar-brand {
   float: left;
@@ -105,7 +122,6 @@
   padding-left: 40px;
   font-size: 31px;
 }
-
 #menu ul.sub-menus {
   height: auto;
   overflow: hidden;
@@ -184,11 +200,21 @@
 }
 @media screen and (max-width: 800px) {
   #menu {
-    background: #0099cc;
-    color: #fff;
+    background: #ffffff;
+    color: rgb(0, 0, 0);
     height: 59px;
     padding-left: 19px;
-    border-radius: 5px;
   }
+}
+@media only screen and (max-width: 480px) {
+  .logo-titel {
+    display: none;
+  }
+  .mobile-titel {
+    display: block;
+  }
+  .nav-menu-items {
+  margin-left: 0px;
+}
 }
 </style>
