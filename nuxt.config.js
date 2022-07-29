3e364dcb86f496938435b1e38f3bccc055bf7e4d
pezhumalai <pezhumalai501@gmail.com>
2022-07-29 12:37:33 +0530

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'images/logo-s.png' }
+      { rel: 'icon', type: 'image/x-icon', href: 'images/logo-stu.png' }
     ]
   },
 
